9a1cbe0d1b6fb310818d28e3f7737982400d5880
krittaboon <krittaboon2541@gmail.com>
2020-04-16 12:38:10 +0700

    fix image upload2

--- Web_Prog_Project/urls.py
+++ Web_Prog_Project/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('<slug:program_slug>/book',views.programBook,name='program_book')
 ]
 
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
 
 if settings.DEBUG :
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
