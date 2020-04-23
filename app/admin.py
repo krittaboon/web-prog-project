b4864dc31ea528d22594056416b870a68744a79d
krittaboon <krittaboon2541@gmail.com>
2020-04-23 15:33:48 +0700

    add order to admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from app.models import Program,ProgramDetail
+from app.models import Program,ProgramDetail,Order
 
 # Register your models here.
 admin.site.register(Program)
 admin.site.register(ProgramDetail)
+admin.site.register(Order)
